de24f2431a1dc6cc3369cd2eccad5e87f606e652
My Name <cpgsom819@gmail.com>
2023-02-09 11:06:34 +0800

    increase added

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 
-document.getElementById('btn-case-plus').addEventListener('click', function(){
 
 
-    console.log('case button clicked')
+function updateCaseNumber(isIncrease){
 
     const caseNumberField = document.getElementById('case-number-field');
-
     const caseNumberString = caseNumberField.value;
-
     const previouscaseNumber = parseInt(caseNumberString);
 
+    let  newCaseNumber;
 
-    const newCaseNumber = previouscaseNumber + 1;
+    if(isIncrease){
+        newCaseNumber = previouscaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previouscaseNumber - 1;
+    }
+
 
     caseNumberField.value = newCaseNumber;
 
-    
 
+}
+
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+
+
+    updateCaseNumber(true)
+})
+
+
+document.getElementById("btn-case-minus").addEventListener('click', function(){
+
+
+    updateCaseNumber(false)
 
 
 
